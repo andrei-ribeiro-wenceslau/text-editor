e7f2c913f347bf889533c69cc6bc2def688951fa
Andrei <andrei.ribeirow@gmail.com>
2023-10-03 23:36:49 +1100

    Fixing issues with Heroku database

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,42 +1,50 @@
-import { openDB } from "idb";
+import { openDB } from 'idb';
 
 const initdb = async () =>
-  openDB("jate", 1, {
+  openDB('jate', 1, {
     upgrade(db) {
-      if (db.objectStoreNames.contains("jate")) {
-        console.log("jate database already exists");
+      if (db.objectStoreNames.contains('jate')) {
+        console.log('jate database already exists');
         return;
       }
-      db.createObjectStore("jate", { keyPath: "id", autoIncrement: true });
-      console.log("jate database created");
+      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
+      console.log('jate database created');
     },
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
-  const jateDb = await openDB("jate", 1);
-  const tx = jateDb.transaction("jate", "readwrite");
-  const store = tx.objectStore("jate");
-  const request = store.put({ id: 1, value: content });
-  const result = await request;
+  console.log('Post To The Database...');
 
-  console.log("Data saved to the database", result);
+  const textDb = await openDB('jate', 1);
+
+  const tx = textDb.transaction('jate', 'readwrite');
+
+  const store = tx.objectStore('jate');
+
+  const request = store.put({ id: 1, value: content });
+
+  const result = await request;
+  console.log('Data added to the database....', result);
 };
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
-  try{
-  console.log("GET from the database");
+  console.log('GET from the database');
 
-  const jateDB = await openDB("jate", 1);
-  const tx = jateDB.transaction("jate", "readonly");
-  const store = tx.objectStore("jate");
-  const result = await store.get(1);
-  return result.value;
-}catch {
-  console.error('Error getting data from the database', error);
-  throw error;
+  const textDb = await openDB('jate', 1);
+
+  const tx = textDb.transaction('jate', 'readonly');
+
+  const store = tx.objectStore('jate');
+
+  const request = store.getAll();
+
+  const result = await request;
+
+  console.log('result.value', result);
+
+  return result?.value
 }
-};
 
 initdb();
