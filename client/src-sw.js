851c45d4482ec389da11482d77ede6f2272b018f
Andrei <andrei.ribeirow@gmail.com>
2023-10-03 22:39:25 +1100

    Pushing to Heroku fixes

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,5 +1,5 @@
 const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
-const { CacheFirst } = require('workbox-strategies');
+const { CacheFirst, StaleWhileRevalidate } = require('workbox-strategies');
 const { registerRoute } = require('workbox-routing');
 const { CacheableResponsePlugin } = require('workbox-cacheable-response');
 const { ExpirationPlugin } = require('workbox-expiration');
@@ -26,13 +26,14 @@ warmStrategyCache({
 
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
-// TODO: Implement asset caching
+// Implement asset caching
 registerRoute(
   ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
-
   new StaleWhileRevalidate({
-    cacheName: 'assets-cache',
+    // name of the cache storage
+    cacheName: 'asset-cache',
     plugins: [
+      // cache responses with these headers to a maximum-age of 30 days
       new CacheableResponsePlugin({
         statuses: [0, 200],
       }),
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,65 +1,70 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const WebpackPwaManifest = require("webpack-pwa-manifest");
-const path = require("path");
-const { InjectManifest } = require("workbox-webpack-plugin");
-
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackPwaManifest = require('webpack-pwa-manifest');
+const path = require('path');
+const { InjectManifest } = require('workbox-webpack-plugin');
 
 module.exports = () => {
   return {
-    mode: "production",
+    mode: 'production',
     entry: {
-      main: "./src/js/index.js",
-      install: "./src/js/install.js",
+      main: './src/js/index.js',
+      install: './src/js/install.js',
     },
     output: {
-      filename: "[name].bundle.js",
-      path: path.resolve(__dirname, "dist"),
+      filename: '[name].bundle.js',
+      path: path.resolve(__dirname, 'dist'),
     },
+    // Add and configure workbox plugins for a service worker and manifest file.
     plugins: [
+      // webpack plugin to generate HTML
       new HtmlWebpackPlugin({
-        template: "./index.html",
-        filename: "index.html",
+        template: './index.html',
+        title: 'Just Another Text Editor',
       }),
-
-      // Configure WebpackPwaManifest plugin for manifest file
+      // service worker
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
+      }),
+      // manifest.json
       new WebpackPwaManifest({
-        name: "Text Editor",
-        short_name: "TE",
-        description: "Text Editor App",
-        background_color: "#ffffff",
-        theme_color: "#000000",
+        fingerprints: false,
+        inject: true,
+        name: 'Just Another Text Editor',
+        short_name: 'JATE',
+        description:
+          'text editor that runs in the browser and functions offline',
+        background_color: '#225ca3',
+        theme_color: '#225ca3',
+        start_url: '/',
+        publicPath: '/',
         icons: [
           {
-            src: path.resolve("src/images/logo.png"),
+            src: path.resolve('src/images/logo.png'),
             sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
           },
         ],
       }),
-
-      // Configure InjectManifest plugin for service worker
-      new InjectManifest({
-        swSrc: "./src-sw.js", // Replace with your service worker script path
-        swDest: "src-sw.js",
-      }),
     ],
-
+    // Add CSS loaders and babel to webpack.
     module: {
       rules: [
-        // Add CSS loader rule
         {
           test: /\.css$/i,
-          use: ["style-loader", "css-loader"],
+          use: ['style-loader', 'css-loader'],
         },
-        // Add Babel loader rule
         {
-          test: /\.js$/,
+          test: /\.m?js$/,
           exclude: /node_modules/,
           use: {
-            loader: "babel-loader",
+            loader: 'babel-loader',
             options: {
-              presets: ["@babel/preset-env"],
+              presets: ['@babel/preset-env'],
+              plugins: [
+                '@babel/plugin-proposal-object-rest-spread',
+                '@babel/transform-runtime',
+              ],
             },
           },
         },
